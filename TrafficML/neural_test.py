3df202aa59010b38b76cd9362d4e9cddd234bef8
kalyons11 <kalyons@mit.edu>
2017-03-24 10:31:17 -0400

    Working on custom CNN loss function and model output.

--- TrafficML/neural_test.py
+++ TrafficML/neural_test.py
@@ -1,59 +1,52 @@
 # 0. Load output JSON files.
 
-import glob, json, time, numpy as np
-from keras.models import Sequential
+import glob, json, time, numpy as np, pickle, os, tensorflow as tf
+from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Dropout, Flatten, Reshape
-from keras.constraints import maxnorm
-from keras.optimizers import SGD
-from keras.layers.convolutional import Conv2D
-from keras.utils import np_utils
-from keras import backend as K
-K.set_image_dim_ordering('th')
+from keras.layers.convolutional import Convolution2D
 
-directory = '../../../data/train/*.json'
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
 
-files = glob.glob(directory)
+# Data methods
 
-# 1. Convert files to input and output feature vectors.
+def normalize(data):
+    return np.array(data) / max(np.max(data), np.abs(np.min(data)))
 
-train_inp = []
-train_out = []
+def residuals(expected, predicted):
+    return expected - predicted
+    
+def normalized_residuals(expected, predicted):
+    return residuals(normalize(expected), normalize(predicted))
+    
+def total_sum_squares(expectedVals):
+    mean = np.mean(expectedVals)
+    
+    diffSum = [np.sum(expected - mean)**2 for expected in expectedVals]
+    return np.sum(diffSum)
+    
+def residual_sum_squares(expectedVals, predictedVals):
+    assert len(expectedVals) == len(predictedVals)
+    
+    res = []
+    for i in range(len(expectedVals)):
+        res.append(residuals(expectedVals[i], predictedVals[i]))
+    return np.sum(np.array(res)**2)
+    
+def R_squared(expectedVals, predictedVals):
+    sumRes = residual_sum_squares(expectedVals, predictedVals)
+    sumTot = total_sum_squares(predictedVals)
+    
+    return 1 - (sumRes / sumTot)
 
-POP_ARR = [5, 8, 16, 16, 23, 59]
+# 1. Load data from pickle file.
 
-def get_population(t, density_array):
-	if t not in range(0, 6):
-		return 0
-	return density_array[t] * POP_ARR[t]
-
-for name in files:
-	with open(name, 'r') as f:
-		current_input = []
-		current_output = []
-		d = json.loads(f.read())
-		max_traffic = max([cell['data']['traffic'] for cell in d['grid'] if 'data' in cell])
-		max_wait = max([cell['data']['wait'] for cell in d['grid'] if 'data' in cell])
-		if max_traffic == 0 or max_wait == 0:
-			continue
-		for cell in d['grid']:
-			current_input.append(get_population(cell['type'], d['objects']['density']))
-			current_input.append(int(cell['type'] == 6))
-			if 'data' in cell:
-				current_output.append(cell['data']['traffic'])
-				current_output.append(cell['data']['wait'])
-			else:
-				current_output.append(0)
-				current_output.append(0)
-		train_inp.append(np.array(current_input))
-		train_out.append(np.array(current_output))
-
-# Reshaping data into 2D matrix form
-
-train_inp = np.array(train_inp)
-train_out = np.array(train_out)
-
-train_inp = train_inp.reshape((-1, 16, 16, 2))
-train_out = train_out.reshape((-1, 16, 16, 2))
+data = pickle.load(open('data.p', 'rb'))
+train_x = data['train_x'].reshape((-1, 16, 16, 2))
+train_y = data['train_y'].reshape((-1, 16, 16, 2))
+test_x = data['test_x'].reshape((-1, 16, 16, 2))
+test_y = data['test_y'].reshape((-1, 16, 16, 2))
 
 # 2. Create model. Convolutional neural network.
 
@@ -61,36 +54,86 @@ start_time = time.time()
 print('Compiling Model ... ')
 
 model = Sequential()
-model.add(Conv2D(64, 3, input_shape=(16, 16, 2), nb_col=1))
-model.add(Dropout(0.2))
-# model.add(MaxPooling1D(pool_length=4))
-model.add(Flatten())
-# model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
+model.add(Convolution2D(64, 3, input_shape=(16, 16, 2), nb_col=1))
 # model.add(Dropout(0.5))
-model.add(Dense(512, activation='linear'))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
 model.add(Reshape((16, 16, 2)))
 
 # 3. Compile model.
 
-epochs = 25
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-print(model.summary())
+def loss(y_true, y_pred):
+	with sess.as_default():
+		print(y_true.get_shape())
+
+model.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
 
 print('Model compield in {0} seconds'.format(time.time() - start_time))
 
 # 4. Train the network.
+
 start_time = time.time()
 print('Training model...')
 
-model.fit(train_inp, train_out, nb_epoch=200, batch_size=20, verbose=1)
+model.fit(train_x, train_y, nb_epoch=5, batch_size=128, verbose=1)
 
 print("Training duration : {0}".format(time.time() - start_time))
 
-# 5. Test network on new data. Make predictions and compare cities. ***
+# Serialize model to JSON
+# Taken from http://machinelearningmastery.com/save-load-keras-deep-learning-models/
+
+
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# Serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk.")
+
+
+
+# Load model from JSON
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk.")
+
+# 5. Test network on train data. Check that everything is okay. ***
+
+predicted_first_city = model.predict(train_x[0].reshape(-1, 16, 16, 2))[0]
+
+def compare(inp, one, two):
+	# Get R^2 between two cities, only based on road cells...
+	print(inp.shape)
+	print(one.shape)
+	print(two.shape)
+	first_traffic = []
+	second_traffic = []
+	first_wait = []
+	second_wait = []
+	for row in range(inp.shape[0]):
+		for col in range(inp.shape[1]):
+			if inp[row][col][1] == 1:
+				# Is a road.
+				first_traffic.append(one[row][col][0])
+				second_traffic.append(two[row][col][0])
+				first_wait.append(one[row][col][1])
+				second_wait.append(two[row][col][1])
+	print(first_traffic)
+	print(second_traffic)
+	return (R_squared(first_traffic, second_traffic), R_squared(first_wait, second_wait))
+
+a, b = compare(train_x[0], train_y[0], predicted_first_city)
+
+print(a, b)
 
 # score = model.evaluate(X_test, y_test, batch_size=16)
 
 # print("Network's test score [loss, accuracy]: {0}".format(score))
+
+print("Process complete!")
