a7e7693041c596159ec96da7764582356870000e
kalyons11 <kalyons@mit.edu>
2017-04-04 12:24:36 -0400

    Fixing PEV file issue. Neural updates.

--- TrafficML/neural_test.py
+++ TrafficML/neural_test.py
@@ -1,16 +1,17 @@
 # 0. Load output JSON files.
 
-import glob, json, time, numpy as np, pickle, os, tensorflow as tf
-from keras.models import Sequential, model_from_json
-from keras.layers import Dense, Dropout, Flatten, Reshape
-from keras.layers.convolutional import Convolution2D
+import glob, json, numpy as np, pickle, os, matplotlib.pyplot as plt, tensorflow as tf
+from keras.models import Model, model_from_json
+from keras.layers import Input, Dense, Flatten, Reshape
+from keras.layers.merge import Concatenate
+from keras.layers.convolutional import Conv2D
+import keras.backend as K
 
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+"""
+Statistical extraction functions.
 
-# Data methods
-
+Each expect raw data, nothing to do with the city structure
+"""
 def normalize(data):
     return np.array(data) / max(np.max(data), np.abs(np.min(data)))
 
@@ -50,39 +51,66 @@ test_y = data['test_y'].reshape((-1, 16, 16, 2))
 
 # 2. Create model. Convolutional neural network.
 
-start_time = time.time()
 print('Compiling Model ... ')
 
-model = Sequential()
-model.add(Convolution2D(64, 3, input_shape=(16, 16, 2), nb_col=1))
-# model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Reshape((16, 16, 2)))
+inp = Input(shape=(16, 16, 2), name='input_layer')
+x = Conv2D(64, (4, 4), activation='relu', input_shape=(16, 16, 2))(inp)
+x = Flatten()(x)
+x = Dense(512, activation='relu')(x)
+x = Reshape((16, 16, 2))(x)
+y = Concatenate()([x, inp])
+
+model = Model(inputs=inp, outputs=y)
+# print(model.summary())
 
 # 3. Compile model.
 
-def loss(y_true, y_pred):
-	with sess.as_default():
-		print(y_true.get_shape())
+def custom_loss(y_true, y_pred):
+	y_pred, inp = tf.split(y_pred, 2, 3)
+	# y_true is (16, 16, 2) - traffic and wait for each cell
+	# y_pred is (16, 16, 2) - traffic and wait for each cell
+	# inp is (16, 16, 2) - population and isroad for each cell
+	_, road = tf.split(inp, 2, 3)
+	# road is (16, 16, 1) - 1 if road, else 0
+	# now, repeat road once along axis dim = 2
+	road = tf.tile(road, [1, 1, 1, 2])
+	# road is now (16, 16, 2)
+	diff = tf.subtract(y_true, y_pred)
+	sq = tf.square(diff)
+	mul = tf.multiply(road, sq)
+	error = tf.reduce_sum(mul) / tf.reduce_sum(road)
+	return error
 
-model.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
+def custom_accuracy(y_true, y_pred):
+	y_pred, inp = tf.split(y_pred, 2, 3)
+	# y_true is (16, 16, 2) - traffic and wait for each cell
+	# y_pred is (16, 16, 2) - traffic and wait for each cell
+	# inp is (16, 16, 2) - population and isroad for each cell
+	_, road = tf.split(inp, 2, 3)
+	# road is (16, 16, 1) - 1 if road, else 0
+	# now, repeat road once along axis dim = 2
+	road = tf.tile(road, [1, 1, 1, 2])
+	# road is now (16, 16, 2)
+	sim = tf.to_float(tf.multiply(road, y_true))
+	pred = tf.to_float(tf.multiply(road, y_pred))
+	diff = tf.abs(tf.subtract(sim, pred))
+	denom_raw = tf.maximum(sim, pred)
+	cond = tf.equal(tf.zeros_like(denom_raw), denom_raw)
+	ones = tf.ones_like(denom_raw)
+	denom = tf.where(cond, ones, denom_raw)
+	errors = tf.divide(diff, denom)
+	return 1 - tf.reduce_mean(errors)
 
-print('Model compield in {0} seconds'.format(time.time() - start_time))
+model.compile(loss=custom_loss, optimizer='adam', metrics=[custom_accuracy])
 
 # 4. Train the network.
 
-start_time = time.time()
-print('Training model...')
-
-model.fit(train_x, train_y, nb_epoch=5, batch_size=128, verbose=1)
-
-print("Training duration : {0}".format(time.time() - start_time))
+# model.fit(train_x, train_y, epochs=20, batch_size=96, verbose=1)
 
 # Serialize model to JSON
 # Taken from http://machinelearningmastery.com/save-load-keras-deep-learning-models/
 
-
+'''
 
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
@@ -91,7 +119,7 @@ with open("model.json", "w") as json_file:
 model.save_weights("model.h5")
 print("Saved model to disk.")
 
-
+'''
 
 # Load model from JSON
 
@@ -103,15 +131,18 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk.")
 
+model = loaded_model
+
+
+
 # 5. Test network on train data. Check that everything is okay. ***
 
-predicted_first_city = model.predict(train_x[0].reshape(-1, 16, 16, 2))[0]
+index = 256
+
+predicted_first_city = model.predict(train_x[index].reshape(-1, 16, 16, 2))[0]
 
 def compare(inp, one, two):
 	# Get R^2 between two cities, only based on road cells...
-	print(inp.shape)
-	print(one.shape)
-	print(two.shape)
 	first_traffic = []
 	second_traffic = []
 	first_wait = []
@@ -124,13 +155,49 @@ def compare(inp, one, two):
 				second_traffic.append(two[row][col][0])
 				first_wait.append(one[row][col][1])
 				second_wait.append(two[row][col][1])
-	print(first_traffic)
-	print(second_traffic)
 	return (R_squared(first_traffic, second_traffic), R_squared(first_wait, second_wait))
 
 a, b = compare(train_x[0], train_y[0], predicted_first_city)
 
 print(a, b)
+
+def plot_results(inp, train, predicted):
+
+	# Goal - compare the traffic scores of train data and predicted data...
+
+	pop_matrix = np.zeros((16, 16))
+	train_matrix = np.zeros((16, 16))
+	p_matrix = np.zeros((16, 16))
+
+	maxPop = float(inp.max())
+	maxTraffic_train = float(train.max())
+	maxTraffic_predicted = float(predicted.max())
+
+	print(predicted.shape)
+
+	for i in range(16):
+		for j in range(16):
+			if inp[i][j][1] == 1:
+				# Road cell - we want to plot traffic data here
+				pop_matrix[i][j] = -1
+				train_matrix[i][j] = train[i][j][0] / maxTraffic_train
+				p_matrix[i][j] = predicted[i][j][0] / maxTraffic_predicted
+			else:
+				pop_matrix[i][j] = inp[i][j][0] / maxPop
+				train_matrix[i][j], p_matrix[i][j] = -1, -1
+
+	_, one = plt.subplots()
+	one.imshow(pop_matrix, interpolation='nearest')
+
+	_, two = plt.subplots()
+	two.imshow(train_matrix, interpolation='nearest')
+
+	_, three = plt.subplots()
+	three.imshow(p_matrix, interpolation='nearest')
+
+plot_results(train_x[index], np.delete(train_y[index], 0, 2), np.delete(predicted_first_city, 0, 2))
+
+plt.show('hold')
 
 # score = model.evaluate(X_test, y_test, batch_size=16)
 
