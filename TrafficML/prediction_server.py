9d26f940a37d199e6fc0e681d0b68295038c1af7
kalyons11 <kalyons@mit.edu>
2017-05-12 14:19:37 -0400

    Updating prediction server with linreg.

--- TrafficML/prediction_server.py
+++ TrafficML/prediction_server.py
@@ -4,10 +4,9 @@
     Date created: 5/12/2017
     Date last modified: 5/12/2017
     Python Version: 3.5
-    Purpose: Developing a simple UDP server that can send and receive City objects and run machine learning prediction algorithms. We will combine linear regression on traffic features with a CNN prediction on wait time features.
+    Purpose: Developing a simple UDP server that can send and receive City objects and run machine learning prediction algorithms. We will combine linear regression on traffic features with a CNN prediction on wait time features. Implements the base city_udp class and applies custom logic in loop format to make predictions.
     TO DO:
-   	- Get linear model pickle file and specs from Alex.
-   	- Implement logging functionality.
+   	- Implement general/scalable logging functionality.
 '''
 
 # Generic import statements
@@ -18,23 +17,25 @@ sys.path.extend(['../TrafficTreeSim/', '../'])
 import cityiograph, city_udp, utils, traffic_regression
 
 # Global instance variables
-LINEAR_MODEL_FILENAME = '' # Pickle file for traffic predictor
-ROOT_NN_FILENAME = 'saved_model' # Root name for neural network JSON/H5
+LINEAR_MODEL_FILENAME = './model_files/linear_model.pkl' # Pickle file for traffic predictor
+ROOT_NN_FILENAME = './model_files/neural_model' # Root name for neural network JSON/H5
 LISTENING = True # Bool to say whether or not we want our server to be "on" and listening
 SERVER_NAME = 'Prediction_Server'
 RECEIVE_IP = "127.0.0.1"
 RECEIVE_PORT = 9000
 SEND_IP = "127.0.0.1"
 SEND_PORT = 9001
+NUM_FEATURES = 512
+MATRIX_SHAPE = (-1, 16, 16, 2)
 
 # Create instance of our server
 server = city_udp.City_UDP(SERVER_NAME, receive_port = RECEIVE_PORT, send_port = SEND_PORT)
 
 # Load linear and neural models, respectively
-# linear_model = pickle.load(open(LINEAR_MODEL_FILENAME, 'rb'))
+linear_model = pickle.load(open(LINEAR_MODEL_FILENAME, 'rb'))
 neural_model = utils.deserialize_model(ROOT_NN_FILENAME)
 
-# Constantly loop and wait
+# Constantly loop and wait for new city packets to reach the UDP server
 while LISTENING:
 
 	print("{} listening on ip: {}, port: {}.".format(SERVER_NAME, RECEIVE_IP, RECEIVE_PORT))
@@ -53,22 +54,29 @@ while LISTENING:
 
 	# Separate into input for linear and neural models
 	linear_input = [features]
-	neural_input = features.reshape((-1, 16, 16, 2))
+	neural_input = features.reshape(MATRIX_SHAPE)
 
 	# Make traffic prediction using linear model
-	# linear_output = linear_model.predict(linear_input) # Type = ??? - still waiting on this
+	linear_output = linear_model.predict(linear_input)[0] # Type = np array, 1 x 512
+	linear_output[linear_output < 0] = 0. # Trim negative values from regression
 
 	# Make wait prediction using neural model
 	neural_output = neural_model.predict(neural_input)[0] # Type = np array, shape = (16, 16, 2)
 
+	# Split on traffic and wait values
+	traffic_list = linear_output[::2].tolist() # List of size 256
+	wait_list = neural_output[: , : , 1].reshape(NUM_FEATURES // 2).tolist() # List of size 256
+
 	# Need to combine traffic and wait output values into a final list of length 512
 	# See http://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion for clever combination trick
-	wait_list = neural_output[: , : , 1].reshape(256).tolist() # List of size 256
+	result = np.zeros(NUM_FEATURES)
+	result[::2] = traffic_list
+	result[1::2] = wait_list
 
 	# Write prediction back to the cityiograph.City structure
 	outgoing_city = incoming_city
-	traffic_regression.output_to_city(outgoing_city, result_list)
+	traffic_regression.output_to_city(outgoing_city, result)
 
-	# Send the city back to Grasshopper script via UDP
+	# Send the city object directly back to Grasshopper script via UDP server
 	server.send_city(outgoing_city)
-	print("Updated city sent!")
+	print("Predicted city sent!")
