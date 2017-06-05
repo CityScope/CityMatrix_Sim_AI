6c46e344585a598bcefc70d6abfca0061f813153
kalyons11 <kalyons@mit.edu>
2017-06-04 20:33:11 -0400

    Removing bad log statement.

--- global/setup.py
+++ global/setup.py
@@ -3,7 +3,7 @@ Filename: setup.py
 Author: kalyons11 <mailto:kalyons@mit.edu>
 Created: 2017-06-04 20:23:55
 Last modified by: kalyons11
-Last modified time: 2017-06-04 20:30:21
+Last modified time: 2017-06-04 20:32:55
 Description:
 	- General setup.py script to be run before we start the server for the first time.
 TODO:
@@ -19,5 +19,5 @@ DIRECTORY_LIST = [ INPUT_CITIES_DIRECTORY, OUTPUT_CITIES_DIRECTORY, GAMA_OUTPUT_
 
 for d in DIRECTORY_LIST:
     if not os.path.exists(d):
-        log.warn("Creating new directory {}.".format(d))
+        print("Creating new directory {}.".format(d))
         os.makedirs(d)
