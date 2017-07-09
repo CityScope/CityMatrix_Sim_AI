607efc87dd4148e57ff45332158483736e6f9ae7
kalyons11 <kalyons@mit.edu>
2017-07-08 23:02:28 -0400

    Setup file.

--- global/setup.py
+++ global/setup.py
@@ -1,20 +1,17 @@
-'''
-Filename: setup.py
-Author: kalyons11 <mailto:kalyons@mit.edu>
-Created: 2017-06-04 20:23:55
-Last modified by: kalyons11
-Description:
-    - General setup.py script to be run before we start the server for the first time.
-TODO:
-    - None at this time.
-'''
+"""
+General setup.py script to be run before we start the server for the first time.
+"""
 
 import os
-from config import *
+import config
 
 # First, ensure that all directories exist
-# Taken from http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
-DIRECTORY_LIST = [ INPUT_CITIES_DIRECTORY, PREDICTED_CITIES_DIRECTORY, GAMA_OUTPUT_DIRECTORY, XML_DIRECTORY ]
+# Taken from
+# http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
+DIRECTORY_LIST = [config.INPUT_CITIES_DIRECTORY,
+                  config.PREDICTED_CITIES_DIRECTORY,
+                  config.GAMA_OUTPUT_DIRECTORY,
+                  config.XML_DIRECTORY]
 
 for d in DIRECTORY_LIST:
     if not os.path.exists(d):
