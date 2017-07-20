f79f04cd11fe2fd2673b67f809048b963a22bf21
kalyons11 <kalyons@mit.edu>
2017-07-19 21:36:22 -0400

    Latest neural net code.
    The new layout of MachineLearning/neural_cnn.py is a basic
    convolutional neural network experiment.
    
    This model was able to achieve an R^2 score of 0.82 on the
    validation set of our latest city round.
    
    Further analysis is needed to verify these results, however.
    
    Architectural changes can be made to improve the performance
    of this model, for sure.

--- MachineLearning/neural_cnn.py
+++ MachineLearning/neural_cnn.py
@@ -1,141 +1,177 @@
-# Global imports
-import glob, json, numpy as np, pickle, os, tensorflow as tf, sys, os
+"""
+File to run our neural network on the data.
+"""
 
-# Keras imports
-from keras.models import Model, model_from_json
-from keras.layers import Input, Dense, Flatten, Reshape
-from keras.layers.merge import Concatenate
-from keras.layers.convolutional import Conv2D
+import os
+import sys
+from glob import glob
+
 import keras.backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+import sacred
+from dotmap import DotMap
+from keras.callbacks import TensorBoard
+from keras.layers import (BatchNormalization, Conv2D, Dense, Dropout, Flatten,
+                          Input, Reshape)
+from keras.models import Model
+from keras.regularizers import l1
+from keras_tqdm import TQDMCallback
+from sacred.observers import FileStorageObserver
+from tqdm import tqdm
 
-# Custom local imports
 sys.path.append('../global/')
-import utils
-from config import *
-# from neural_server import serialize_model, deserialize_model
-# from traffic_regression import output_to_city
+import cityiograph
 
-# Variable declarations
-TRAIN_DATA_FILENAME = '../../../data/no_wandering_data_train.p'
-TEST_DATA_FILENAME = '../../../data/no_wandering_data_test.p'
-PREDICTION_OUTPUT_DIR = 'no_wandering_neural_predictions/'
-DO_RUN = False
-ROOT_FILENAME = "no_wandering_cnn"
+# Define experiment
+ex = sacred.Experiment()
 
-# Create prediction dir if does not already exist
-if not os.path.exists(PREDICTION_OUTPUT_DIR):
-    os.makedirs(PREDICTION_OUTPUT_DIR)
+# Add observers
+ex.observers.append(FileStorageObserver.create('runs'))
 
-# 1. Load data from pickle files.
-print("Loading data...")
 
-train_cities, train_filenames, train_x, train_y = pickle.load(open(TRAIN_DATA_FILENAME, 'rb'))
-test_cities, test_filenames, test_x, test_y = pickle.load(open(TEST_DATA_FILENAME, 'rb'))
+# http://jmbeaujour.com/ml/2017/03/20/CoeffDetermination_CustomMetric4Keras/
+def coeff_determination(y_true, y_pred):
+    SS_res =  K.sum(K.square( y_true-y_pred )) 
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) ) 
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
-print(len(test_cities), len(test_filenames), len(test_x), len(test_y))
 
-train_x = train_x.reshape((-1, 16, 16, 2))
-train_y = train_y.reshape((-1, 16, 16, 2))
-test_x = test_x.reshape((-1, 16, 16, 2))
-test_y = test_y.reshape((-1, 16, 16, 2))
+def configurations():
+    config = DotMap()
 
-print("Successfully loaded data.")
+    # Data configs
+    config.dir = '../../../data/new_traffic_cities/*/*.json'
 
-def custom_loss(y_true, y_pred):
-    y_pred, inp = tf.split(y_pred, 2, 3)
-    # y_true is (16, 16, 2) - traffic and wait for each cell
-    # y_pred is (16, 16, 2) - traffic and wait for each cell
-    # inp is (16, 16, 2) - population and isroad for each cell
-    _, road = tf.split(inp, 2, 3)
-    # road is (16, 16, 1) - 1 if road, else 0
-    # now, repeat road once along axis dim = 2
-    road = tf.tile(road, [1, 1, 1, 2])
-    # road is now (16, 16, 2)
-    diff = tf.subtract(y_true, y_pred)
-    sq = tf.square(diff)
-    mul = tf.multiply(road, sq)
-    error = tf.reduce_sum(mul) / tf.reduce_sum(road)
-    return error
+    # Hyperparams
+    config.hyper.lr = 1e-3
+    config.hyper.optimizer = 'adam'
+    config.hyper.dropout = 0.3
+    config.hyper.lam = 1e-5
+    config.hyper.loss = 'mean_squared_error'
+    config.hyper.metrics = [coeff_determination]
+    config.hyper.batch_size = 32
+    config.hyper.nb_epochs = 500
+    config.hyper.nb_filters = 4
+    config.hyper.batch_norm = False
+    config.hyper.num_dense_units = 512
+    config.hyper.num_dense_layers = 1
 
-def custom_accuracy(y_true, y_pred):
-    y_pred, inp = tf.split(y_pred, 2, 3)
-    # y_true is (16, 16, 2) - traffic and wait for each cell
-    # y_pred is (16, 16, 2) - traffic and wait for each cell
-    # inp is (16, 16, 2) - population and isroad for each cell
-    _, road = tf.split(inp, 2, 3)
-    # road is (16, 16, 1) - 1 if road, else 0
-    # now, repeat road once along axis dim = 2
-    road = tf.tile(road, [1, 1, 1, 2])
-    # road is now (16, 16, 2)
-    sim = tf.to_float(tf.multiply(road, y_true))
-    pred = tf.to_float(tf.multiply(road, y_pred))
-    diff = tf.abs(tf.subtract(sim, pred))
-    denom_raw = tf.maximum(sim, pred)
-    cond = tf.equal(tf.zeros_like(denom_raw), denom_raw)
-    ones = tf.ones_like(denom_raw)
-    denom = tf.where(cond, ones, denom_raw)
-    errors = tf.divide(diff, denom)
-    return 1 - tf.reduce_mean(errors)
+    # Debug/util configs
+    config.util.print_model = True
+    config.util.val_split = 0.15
 
-if DO_RUN:
-    # 2. Create model. Convolutional neural network.
-    print('Compiling Model ... ')
+    return config
 
-    inp = Input(shape=(16, 16, 2), name='input_layer')
-    x = Conv2D(128, (5, 5), activation='relu', input_shape=(16, 16, 2))(inp)
-    # x = Conv2D(32, (3, 3), activation='relu', input_shape=(12, 12, 128))(x)
-    x = Flatten()(x)
-    x = Dense(512, activation='relu')(x)
-    x = Reshape((16, 16, 2))(x)
-    # y = Concatenate()([x, inp])
 
-    model = Model(inputs=inp, outputs=x)
-    print(model.summary())
+@ex.config
+def sacred_configurations():
+    config = configurations()
 
-    # 3. Compile model.
-    print("Compiling model.")
-    model.compile(loss = LOSS_FUNCTION, optimizer = OPTIMIZER, metrics = KERAS_METRICS)
 
-    # 4. Train the network.
-    print("Fitting model.")
-    model.fit(train_x, train_y, epochs = 12, batch_size = 128, verbose = 1)
+@ex.capture
+def data(config):
+    print(">>> Loading data...")
 
-    # Serialize model to file
-    # utils.serialize_model(model, ROOT_FILENAME)
-    # print("Serialized!!!")
-else:
-    # Or, deserialize model from file
-    model = utils.deserialize_model(ROOT_FILENAME)
-    model.compile(loss = LOSS_FUNCTION, optimizer = OPTIMIZER, metrics = KERAS_METRICS)
-    print("Deserialized!!!")
+    config = DotMap(config)
 
-# score = model.evaluate(test_x, test_y, batch_size = 16)
-# print("Network's test score [loss, accuracy]: {0}".format(score))
+    X, Y = [], []
 
-print("Predicting test cities...")
+    for path in tqdm(np.random.permutation(glob(config.dir))):
+        with open(path, 'r') as f:
+            # Extract
+            city = cityiograph.City(f.read())
+            inp = cityiograph.get_features(city, mode='traffic')
+            out = cityiograph.get_results(city, mode='traffic')
+            # Reshape
+            inp = inp.reshape(16, 16, 2)
+            out = out.reshape(16, 16, 2)
+            # Noramlize
+            inp = (inp - inp.mean()) / inp.std()
+            out = (out - out.mean()) / out.std()
+            # Load
+            X.append(inp)
+            Y.append(out)
 
-predictions = model.predict(test_x)
+    X = np.array(X)
+    Y = np.array(Y)
 
-print(predictions.shape)
+    return X, Y
 
-# Write predictions to new JSON files in PREDICTION_OUTPUT_DIR
 
-for i, city in enumerate(test_cities):
-    output_filename = PREDICTION_OUTPUT_DIR + test_filenames[i] + '_prediction.json' # Filename formatting
-    output_list = predictions[i].reshape(512).tolist() # Convert matrix to list
-    utils.output_to_city(city, output_list) # Update city with these values
-    json_string = city.to_json() # Convert to JSON string
-    with open(output_filename, 'w') as f: # Write new JSON prediction to file
-        f.write(json_string)
+@ex.capture
+def model(config):
+    print(">>> Loading model...")
 
-print("Wrote all predictions to {}.".format(PREDICTION_OUTPUT_DIR))
+    config = DotMap(config)
 
-# Compare results with R^2 analysis
+    inp = Input(shape=(16, 16, 2),
+                name='input')
 
-# from neural_compare import *
+    x = Conv2D(filters=config.hyper.nb_filters,
+               kernel_size=(3, 3),
+               activation='relu')(inp)
+    if config.hyper.batch_norm:
+        x = BatchNormalization()(x)
 
-# sim = test_y.reshape((-1, 512))
-# pred = predictions.reshape((-1, 512))
-# compare_outputs(sim, pred)
+    x = Conv2D(filters=config.hyper.nb_filters * 2,
+               kernel_size=(3, 3),
+               activation='relu')(x)
+    if config.hyper.batch_norm:
+        x = BatchNormalization()(x)
 
-print("Process complete!")
+    x = Conv2D(filters=config.hyper.nb_filters * 3,
+               kernel_size=(3, 3),
+               activation='relu')(x)
+    if config.hyper.batch_norm:
+        x = BatchNormalization()(x)
+
+    dense_layer = Flatten()(x)
+
+    for _ in range(config.hyper.num_dense_layers):
+        dense_layer = Dense(units=config.hyper.num_dense_units,
+                            kernel_regularizer=l1(config.hyper.lam))(dense_layer)
+        dense_layer = Dropout(config.hyper.dropout)(dense_layer)
+        if config.hyper.batch_norm:
+            dense_layer = BatchNormalization()(dense_layer)
+
+    out = Reshape(target_shape=(16, 16, 2))(dense_layer)
+
+    m = Model(inputs=inp, outputs=out)
+
+    if config.util.print_model:
+        print(m.summary())
+
+    return m
+
+
+@ex.automain
+def run(config):
+    config = DotMap(config)
+
+    X, Y = data()
+
+    m = model()
+    m.compile(optimizer=config.hyper.optimizer,
+              loss=config.hyper.loss,
+              metrics=config.hyper.metrics)
+
+    callbacks = [
+        TQDMCallback(),
+        TensorBoard(log_dir=ex.observers[0].dir,
+                    histogram_freq=0,
+                    write_graph=True,
+                    write_images=True)
+    ]
+
+    m.fit(x=X,
+          y=Y,
+          batch_size=config.hyper.batch_size,
+          epochs=config.hyper.nb_epochs,
+          verbose=2,
+          callbacks=callbacks,
+          validation_split=config.util.val_split)
+
+    m.save(os.path.join(ex.observers[0].dir, 'final_model.hdf5'))
+
+    print("Process complete!!!")
